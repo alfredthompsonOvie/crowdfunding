86b08e234b0b3ce0e8f0d6a0f9c7612665ca3c35
thompson <alfredthompsonovie@gmail.com>
2023-02-07 01:12:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="bg"></section>
+  <AppNavigation />
+
+  <router-view></router-view>
+</template>
+
+<script setup>
+import AppNavigation from "@/components/AppNavigation.vue"
+</script>
+
+<style lang="scss" scoped>
+
+</style>
